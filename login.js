ad4054402f6200f01ff30aef4a67fb9ebd12687d
jcryer <jjcryer@mac.com>
2019-11-21 12:46:22 +0000

    Update login.js

--- login.js
+++ login.js
@@ -15,11 +15,10 @@ $(document).ready(function() {
     var room = " ";
     var user = "";
     var connected = false;
-    var peer = " ";
+    var peer;
     var audio;
     var reset = document.getElementById('reset');
     var endWindow = document.getElementById('loseWindow');
-    var lastMessage = -1
 
     var refreshID = setInterval (function() { Check() }, 2000);
 
@@ -29,8 +28,6 @@ $(document).ready(function() {
     reset.onclick = function() {
       location.reload();
     }
-
-    
 
     function Check() {
         if (gameOver == true) {
@@ -43,9 +40,11 @@ $(document).ready(function() {
         }
     }
 
+    
     function InitSockets() {
         console.log("initalizing sockets");
         console.log(peer);
+        
         peer.on('error', err => console.log('error', err));
 
         // POST signal to server, then repeatedly GET
@@ -58,20 +57,6 @@ $(document).ready(function() {
                     data: data
                 }}); 
                 connection.send(json);
-
-                PollForUpdate();
-
-                /*
-                $.post(IP + "/conn/",
-                {
-                    user: user,
-                    room: room,
-                    data: data
-                })
-                .done(function(msg){
-                    PollForUpdate();
-                });
-                */
             }
         });
 
@@ -79,7 +64,7 @@ $(document).ready(function() {
         peer.on('connect', () => {
             connected = true;
             peer.send('connected');
-            connection.send(JSON.stringify({type:"message", purpose:"close"}))
+            connection.send(JSON.stringify({type:"message", purpose:"close"}));
             if (user == "c") {
                 document.onkeydown = CheckKey;
                 $('#gameScreen').hide();
@@ -130,14 +115,13 @@ $(document).ready(function() {
         });
     }
 
+    
     function CheckKey(e) {
 		e = e || window.event;
         if (e.keyCode == '37' || e.keyCode == '38' || e.keyCode == '39' || e.keyCode == '40') {
             peer.send(e.keyCode);
         }
 	}
-
-
 
     $("#creategame").click(function() {
         $( "#mainScreen" ).hide();
@@ -152,139 +136,30 @@ $(document).ready(function() {
         $( "#mainScreen" ).fadeIn();
         $( "#createScreen" ).hide();
     });
-    function PollForUpdate() {
-        var counter = 0;
-        //var refreshPollID = setInterval (function() {
-        if(lastMessage != -1){
-            console.log("signalling");
-            peer.signal(JSON.stringify(lastMessage.data));
-            lastMessage = -1;
-        }
-
-        //}, 10);
-
-        /*
-        var refreshID = setInterval (function() {
-            counter++;
-            if (connected) {
-                clearInterval(refreshID);
-            }else if(counter >= 100){
-                room = "";
-                user = "";
-                connected = false;
-                peer;
-                $( "#mainScreen" ).show();
-                $('#createScreen').hide();
-                $('#gameScreen').hide();
-                $('#incorrect').text("Connection timed out.");
-                $('#incorrect').show();
-
-                clearInterval(refreshID);
-            }
-            $.get(IP + "/conn?user=" + user + "&room=" + room)
-                .done(function(msg){
-                    if (msg == "out") {
-                        clearInterval(refreshID);
-                    }
-                    else if (msg != "fail") {
-                        peer.signal(JSON.stringify(JSON.parse(msg).data));
-
-                        clearInterval(refreshID);
-                    }
-                });
-        }, 500);
-        */
-    }
-
     function GetPin() {
         var json = JSON.stringify({type:"message", purpose:"init", pin:"-1", data:""});
         connection.send(json);
-
-        var refreshGetID = setInterval(function(){
-            if(room != " "){
-                clearInterval(refreshGetID);
-                $( "#gamePin" ).text(room);
-                user = "c";
-                if (peer == " "){
-                    peer = new SimplePeer ({ initiator: true });
-                    InitSockets();
-                }                
-            }
-        },10);
-        
-        
-
-
-        /*
-        connection.sendUTF8()
-        $.get(IP + "/create-room/",
-        function(data,status){
-            $( "#gamePin" ).text(data);
-            room = data;
-            user = "c";
-            peer = new SimplePeer ({ initiator: true });
-            InitSockets();
-        });
-        */
+        peer = new SimplePeer ({ initiator: true });
+        user = "c";
     }
 
     function PostPin(val) {
         var data = " ";
         var json = JSON.stringify({type:"message", purpose:"init", pin:val.toLowerCase(), data:""});
         connection.send(json);
-
-        var refreshPostID = setInterval(function(){
-            if(room != " "){
-                console.log("hello there");
-                clearInterval(refreshPostID);
-                if(room == "-1"){
-                    $('#incorrect').show();
-                    $('#incorrect').text('Incorrect pin.');
-                }else{
-                    $('#incorrect').hide();
-                    $( "#gamePin" ).text("Waiting...");
-                    $( "#pin" ).text("");
-                    console.log("joining");
-                    user = "j";
-                    if(peer == " "){
-                        peer = new SimplePeer();
-                        InitSockets();
-                    }
-                    PollForUpdate(); 
-                }
-
-            }
-        },10);
-
-        
-
-        /*
-        $.post(IP + "/join-room/",
-        {
-            pin: val.toLowerCase()
-        },
-        function(data,status){
-            if (data == "not found") {
-                $('#incorrect').show();
-                $('#incorrect').text('Incorrect pin.');
-            }
-            else {
-                $('#incorrect').hide();
-                $( "#gamePin" ).text("Waiting...");
-                $( "#pin" ).text("");
-                room = val.toLowerCase();
-                user = "j";
-                peer = new SimplePeer();
-                InitSockets();
-                PollForUpdate();
-            }
-        });
-        */
+        user = "j";
     }
 
+    
+    /* 
+    Purpose types:
+    init - starting
+    pass - sending to other client
+    close - ending
+    */
     connection.onmessage = function (message) {
         try {
-            if(peer == " "){
+            if(peer != null){
                 peer = new SimplePeer();
                 InitSockets();
             }
@@ -296,12 +171,18 @@ $(document).ready(function() {
         }
 
         if(data.purpose == "init"){
-            console.log(data.data+" "+data.pin);
-            room = data.pin;
-
-        }else if(data.purpose == "pass"){
-            lastMessage = data.data;
-            PollForUpdate();
+            if (data.room == "-1") {
+                $('#incorrect').show();
+                $('#incorrect').text('Incorrect pin.');
+            }
+            else {
+                $( "#gamePin" ).text(data.pin);
+                room = data.pin;
+            }
+        }
+        else if(data.purpose == "pass"){
+            $('#incorrect').hide();
+            peer.signal(JSON.stringify(data.data));
         }
     }
 
